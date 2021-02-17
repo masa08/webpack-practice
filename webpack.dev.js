a7544c6b5949c738b33be7592bb6ffb0d08ecfe1
masa08 <ushimasarug08@gmail.com>
2021-02-17 21:36:48 +0900

    :sparkles: add webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,14 @@
-const { watch } = require('fs');
 const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.js')
+const commonConfig = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(commonConfig, {
   mode: "development",
   watch: true,
-  devtool: 'cheap-module-eval-source-map'
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    open: true,
+    port: 9000,
+    contentBase: path.resolve(__dirname, 'public'),
+  },
 });
