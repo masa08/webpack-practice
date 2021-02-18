1be3c8c6b41925ab35943dd2dbf05f7e3dd20e78
masa08 <ushimasarug08@gmail.com>
2021-02-18 10:04:23 +0900

    :sparkles: Set target browser

---
+++ public/js/vendor-modules.69ad39cb487ccafee893.js
@@ -0,0 +1,19 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["vendor-modules"],{
+
+/***/ "./src/js/modules/greet.js":
+/*!*********************************!*\
+  !*** ./src/js/modules/greet.js ***!
+  \*********************************/
+/*! exports provided: greet */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "greet", function() { return greet; });
+function greet(name) {
+  return `Hello, ${name}!!`;
+}
+
+/***/ })
+
+}]);
