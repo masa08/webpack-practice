cc26f001d5f2a6548b4ecf1acf0304d21be420aa
masa08 <ushimasarug08@gmail.com>
2021-02-20 19:15:35 +0900

    :sparkles: Add optimize plugin

--- webpack.common.js
+++ webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
       }
     ]
   },
